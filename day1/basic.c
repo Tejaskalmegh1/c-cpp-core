37f6c449e930f71ea66bd84fcc334fd77f9ac2c4
mac <mac@macpro.local>
2025-12-31 17:49:42 +0530

    Feat: add day1 and day2 program

---
+++ day1/basic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int NumOne, NumTwo, Sum;
+    printf("Welcome To BitCode!");
+    NumOne = 10, NumTwo = 20;
+    printf("\nAddition of two Numbers: %d", NumOne + NumTwo);
+
+    return 0;
+}
---
+++ day2/intro_data_types.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int NumberOne ;
+    float NumberTwo = 23.33 ;
+    double NumberThree = 77.777777 ;
+    printf("\n Number One value is : %d",NumberOne);
+    printf("\n Size of NumberOne is  : %lu",sizeof(NumberOne));
+    printf("\n Number Two value is : %f",NumberTwo);
+    printf("\n Number Three value is : %lf",NumberThree);
+    return 0;
+}
