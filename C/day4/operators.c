867b77660a5c42f900a71f491b6f602157ce9f8f
tejaskalmegh1 <er.tejaskalmegh@gmail.com>
2026-01-06 14:46:52 +0530

    feat: add day4 format specifiers

---
+++ C/day4/operators.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+    char x = 'A';
+    short y = 2;
+    printf("\n char X value is: %c",x);
+    printf("\n Size of char x is: %lu",sizeof(x));
+    printf("\n Short y value is: %hd",y);
+    printf("\n Size of short y is: %lu",sizeof(y));
+    
+    return 0;
+}
